449815ce11b07ed1965f17f266fb59ba9e282337
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-08-17 19:00:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun (games = 'roller-skate') {
+    return (`This Saturday, I want to ${games}!`);
+}
+saturdayFun ('bathe my dog')
+
+let mondayWork = function (dayActivity = 'go to the office') {
+    return(`This Monday, I will ${dayActivity}.`);
+}
+mondayWork('work from home')
+
+function wrapAdjective (timesOperator = '*') {
+    return function (sign = 'special') {
+        return (`You are ${timesOperator}${sign}${timesOperator}!`);
+    }
+}
+wrapAdjective()()
